be4a2e2177276df79c4a4e654b81d064ac965467
michaelroesinger <mroesinger@gmail.com>
2022-08-01 08:57:02 -0700

    commit tree 90a4a235eaecf806f07d302cfd50d2f659e3ab17

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName() {
+  upperCaseCustomerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+function changeLeastFavoriteCustomer() {}
+const leastFavoriteCustomer = "stella";
